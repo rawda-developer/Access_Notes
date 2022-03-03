dbc0813bd6533bc60ff16bd1883109def70e8320
Rawda Yasser <rawda.developer@gmail.com>
2022-03-03 18:00:57 +0200

    Feat: add login required to NoteUpdateView & NoteDeleteView & NoteCreateView
    Force the user to login in order to updata, delete or create a note

--- notes/views.py
+++ notes/views.py
@@ -2,35 +2,50 @@ from .forms import NoteCreateForm
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from .models import Note
+
 
 class NoteListView(LoginRequiredMixin, ListView):
     model = Note
     login_url = '/admin'
     template_name = 'notes/notes_list.html'
     context_object_name = 'notes'
+
+
 class NoteDetailView(LoginRequiredMixin, DetailView):
     model = Note
     template_name = 'notes/notes_detail.html'
     context_object_name = 'note'
-class NoteCreateView(CreateView):
+    login_url = '/admin'
+
+
+class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Note
     template_name = 'notes/notes_create.html'
     success_url = '/'
     form_class = NoteCreateForm
+    login_url = '/admin'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
-class NoteDeleteView(DeleteView):
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
     model = Note
     context_object_name = 'note'
     template_name = 'notes/notes_delete.html'
     success_url = '/'
-    
+    login_url = '/admin'
 
-class NoteUpdateView(UpdateView):
+
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
     model = Note
     context_object_name = 'note'
     template_name = 'notes/notes_update.html'
     success_url = '/'
     form_class = NoteCreateForm
-
+    login_url = '/admin'
