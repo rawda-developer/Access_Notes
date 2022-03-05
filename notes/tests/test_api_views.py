4e56c73b1bde847b98adace33d249290a78ce1f4
Rawda Yasser <rawda.developer@gmail.com>
2022-03-05 04:53:46 +0200

    Test: add test case class for destroying notes

--- notes/tests/test_api_views.py
+++ notes/tests/test_api_views.py
@@ -3,7 +3,6 @@ from notes.models import Note
 from django.contrib.auth.models import User
 
 
-
 class NoteCreateTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(
@@ -34,3 +33,27 @@ class NoteCreateTestCase(APITestCase):
 
     def tearDown(self):
         self.user.delete()
+
+
+class NoteDestroyTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='user123', password='password123')
+        self.user.save()
+        self.client.login(username=self.user.username,
+                          password=self.user.password)
+        self.note1 = Note.objects.create(
+            title='Note1', text='Hello world', user=self.user)
+        self.note1.save()
+
+    def test_delete_note(self):
+        note_id = self.note1.id
+        print(note_id)
+        initial_note_count = Note.objects.count()
+        self.client.delete(f'/api/v1/notes/{note_id}/')
+        self.assertEqual(Note.objects.count(), initial_note_count - 1)
+
+    def tearDown(self):
+        self.client.logout()
+        self.user.delete()
+        self.note1.delete()
