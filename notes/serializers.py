751a23d89bf193d9d316831e9bc7e0c64b6c1ba4
Rawda Yasser <rawda.developer@gmail.com>
2022-03-05 06:49:26 +0200

    Fix: add id to note serializer

--- notes/serializers.py
+++ notes/serializers.py
@@ -6,4 +6,4 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ['title', 'text', 'user']
+        fields = ['id', 'title', 'text', 'user']
